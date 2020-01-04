b23628a72d9c7bd36bb2c60bd888adbb4f764683
Matthew Proudman <proudmandandc@gmail.com>
2020-01-04 17:48:05 +0000

    adding to functions to the list.

--- wordblock/addWords.py
+++ wordblock/addWords.py
@@ -83,21 +83,60 @@ class WordsListLayout(GridLayout):
         super().__init__(**kwargs)
 
         self.cols = 2
-        for word in Word().all():
+
+        wordsList = sorted( Word().all(), key = lambda i: i['word'])
+        for word in wordsList:
             
             row = GridLayout()
             row.rows = 1
             row.height = 40
             row.size_hint_y = None
+
+            btn = Button(text=f'delete', size_hint_y=None, height=40, on_press=self.deleteCb)
+            btn.rowId = word['id']
+            btn.word = word['word']
+
             row.add_widget(
                 Label(text=f'{word["word"]}', size_hint_y=None, height=40 )
             )
             row.add_widget(
-                Button(text=f'delete', size_hint_y=None, height=40 )
+                btn
             )
-
-            
             self.add_widget(row)
+
+    def deleteCb(self, inst):
+
+        popup = Popup(
+            title='are you sure you want to delete {}'.format(inst.word),
+            size_hint=(None, None),
+            size=(400, 200),
+        )
+
+        g = GridLayout()
+        g.rows = 1
+
+        delBtn = Button(
+            text='Delete',
+            on_press=lambda i: Word().removeById([inst.rowId]) and popup.dismiss()
+        )
+
+        g.add_widget(
+            delBtn
+        )
+        g.add_widget(
+            Button(
+                text="Cancel",
+                on_press=popup.dismiss
+            )
+        )
+
+        popup.content = g
+        popup.open()
+
+    def delete_suc(self, i):
+        print('deleted')
+        print( self )
+        print( i )
         
 
 class AddWordsApp(App):
@@ -110,11 +149,28 @@ class AddWordsApp(App):
         
         self.wordList = WordsListLayout(spacing=10, size_hint_y=None)
         self.wordList.bind(minimum_height=self.wordList.setter('height'))
-        self.scrollList = ScrollView(size_hint=(1, None), size=(Window.width, 500))
+        self.scrollList = ScrollView(size_hint=(1, None), size=(Window.width, 400))
         self.scrollList.add_widget(self.wordList)
         
+        self.box.add_widget(self.urlPanel)
+        self.box.add_widget(self.addSingle)
+        self.box.add_widget(self.scrollList)
+        self.box.add_widget(Button(text='refresh list', on_press=self.refreshList))
+        return self.box
+
+    def refreshList(self, inst):
+        self.box.clear_widgets()
+
+        self.urlPanel = UrlLayout(size_hint_y=10)
+        self.addSingle = AddSingle(size_hint_y=10)
+        
+        self.wordList = WordsListLayout(spacing=10, size_hint_y=None)
+        self.wordList.bind(minimum_height=self.wordList.setter('height'))
+        self.scrollList = ScrollView(size_hint=(1, None), size=(Window.width, 400))
+        self.scrollList.add_widget(self.wordList)
 
         self.box.add_widget(self.urlPanel)
         self.box.add_widget(self.addSingle)
         self.box.add_widget(self.scrollList)
-        return self.box
+        self.box.add_widget(Button(text='refresh list', on_press=self.refreshList))
+        
