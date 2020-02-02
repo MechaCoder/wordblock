5d923a1ccd6575ccfa27d3831960a288c3ab6832
Matthew Proudman <proudmandandc@gmail.com>
2020-02-02 17:51:14 +0000

    docs added

--- deploy.py
+++ deploy.py
@@ -12,31 +12,39 @@ from json import loads
 class Deploy:
 
     def __init__(self):
+        """ deployment class """
         self.repo = Repo('./')
 
     def getMostRecentTag(self):
+        """returns the most recent tag which should the release"""
         return str(self.repo.tags[0]).replace('.', '')
 
     def getMostRecentCommitHash(self):
+        """returns a sha1 of has of the most recent the last fiveteen chars"""
         return str(self.repo.refs[0].commit.hexsha)[-15:]
 
     def getCurrentBranchName(self):
+        """gets the active Branch"""
         return str(self.repo.active_branch)[8:]
 
     def mkFileName(self):
+        """ builds out the file name of the archive file """
         release = self.getMostRecentTag()
         branch = self.getCurrentBranchName()
         sha_msg = self.getMostRecentCommitHash()
         return f'wordblock_{release}_{branch}_{sha_msg}'
 
     def cpReadmeToBuild(self):
+        """ copy across `readme.md` read me file"""
         return copyfile('./readme.md', './build/readme.md')
 
     def compress(self, fileName: str):
+        """ makes the archive file returns the absute path to the new archive"""
         zipFileAddr = make_archive(f'./{fileName}', 'zip', './build/')
         return zipFileAddr
 
     def postFileToBitbucket(self, filePath: str):
+        """ posts the file to the filepath """
 
         jsonFileObj = open('./devKeys.json')
         json = loads(jsonFileObj.read())
@@ -56,12 +64,15 @@ class Deploy:
     def main(self):
         self.cpReadmeToBuild()
         filename = self.mkFileName()
+
+        print('compressing file.')
         pathname = self.compress(filename)
         rObj = {
             'path': pathname,
             'sentToBb': False
 
         }
+        print('sending file to bitbucket.')
         if self.postFileToBitbucket(pathname) is 200:
             rObj['sentToBb'] = True
 
@@ -71,6 +82,6 @@ class Deploy:
 if __name__ == '__main__':
     x = Deploy().main()
     print('A Deployment .zip has been created;')
-    print(f'>>> {rObj["path"]}')
-    if rObj['sentToBb']:
+    print(f'>>> {x["path"]}')
+    if x['sentToBb']:
         print('Deployment has been sent Bitbucket')
