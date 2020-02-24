6a03b3d8d7eedbb12090bdb71445407aeb12b460
Matthew Proudman <proudmandandc@gmail.com>
2020-02-24 19:22:57 +0000

    ::removed the prints and added a return value

--- wordblock/ai/__init__.py
+++ wordblock/ai/__init__.py
@@ -3,14 +3,12 @@ from .base import AIbase
 
 
 def processAiData():
-    print('started processAIData')
     obj = AIbase()
     obj.processData()
-
-    print('finished processAiData')
     return True
 
 def setPrediction():
     obj = AIbase()
     prodictedid = obj.predict()
     Prefences().set('ai-id', prodictedid)
+    return True
